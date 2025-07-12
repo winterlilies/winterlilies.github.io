9fb145120ad8843ba4d61d5f43fd9a362ab3b4c0
winterlilies <izayoi16nacht@gmail.com>
2025-07-12 03:19:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -159,11 +159,149 @@ ftvt is a tool for visualizing Fourier transforms in one dimension. Signals are 
 <br />
 <h3>Technical challenges</h3>
 <hr />
+<h4>Implementing the FFT</h4>
+<p>The FFT makes use of certain properties of the complex exponential function in order to break down the task recursively and enable an O(n log n) runtime, and so the implementation requires support for complex arithmetic. Javascript lacks built-in support for complex numbers, so I found a workaround by using an ordered pair (actually an array of length 2) to represent the real and imaginary parts of a complex number.</p>
+<pre class="code inBox">
+function cplx(re, im) {
+  return [re, im];
+}
+
+function cplxFromReal(re) {
+  return [re, 0];
+}
+</pre>
+<p>
+All the mathematical functions needed by the algorithm were then implemented manually. Addition and subtraction are simple termwise vector operations. Multiplication and division are slightly more involved, but the formulas can be easily derived.
+</p>
+<pre class="code inBox">
+function cplxAdd(z1, z2) {
+  return cplx(z1[0] + z2[0], z1[1] + z2[1]);
+}
+
+function cplxSub(z1, z2) {
+  return cplx(z1[0] - z2[0], z1[1] - z2[1]);
+}
+
+function cplxMul(z1, z2) {
+  var a = z1[0];
+  var b = z1[1];
+  var c = z2[0];
+  var d = z2[1];
+  var re = a*c - b*d;
+  var im = b*c + a*d;
+  var res = cplx(re, im);
+  return cplx(re, im);
+}
+
+function cplxDiv(z1, z2) {
+  var a = z1[0];
+  var b = z1[1];
+  var c = z2[0];
+  var d = z2[1];
+  var dn = c*c + d*d;
+  var re = (a*c + b*d) / dn;
+  var im = (b*c - a*d) / dn;
+  return cplx(re, im);
+}
+</pre>
+<p>
+A way to compute the complex exponential function can be derived using Euler's formula, so that only the real-valued exponential, sine, and cosine functions are needed. (Alternatively, this formula can be seen as the definition of the complex exponential instead of an equivalent form, if Euler's formula is interpreted as a definition rather than a theorem. But if you see it that way you're cringe)
+</p>
+<pre class="code inBox">
+// e^(x + iy)
+// e^x e^(iy)
+// e^x (cos(y) + i sin(y))
+function cplxExp(z) {
+  var x = z[0];
+  var y = z[1];
+  var ex = Math.exp(x);
+  var re = ex * Math.cos(y);
+  var im = ex * Math.sin(y);
+  return cplx(re, im);
+}
+</pre>
+<p>
+Since Javascript lacks support for operator overloading, the code representing complex valued arithmetic ended up being rather verbose. Given below is the code implementing the Discrete Fourier Transform (DFT), which is a brute force algorithm that follows directly from the definition, and which is used as the base case for the recursive FFT.
+</p>
+<pre class="code inBox">
+function dft_generic(xs, inverse) {
+  var N = xs.length;
+  var ys = [];
+  for (var k = 0; k < N; k++) {
+    var acc = cplxFromReal(0);
+    for (var n = 0; n < N; n++) {
+      var n0 = inverse ? 2 : (-2);
+      var n1 = cplxFromReal(n0);
+      var n2 = cplxMul(n1, cpi);
+      var n3 = cplxMul(n2, j);
+      var n4 = cplxFromReal(n);
+      var n5 = cplxMul(n3, n4);
+      var n6 = cplxFromReal(k);
+      var n7 = cplxMul(n5, n6);
+      var n8 = cplxFromReal(N);
+      var n9 = cplxDiv(n7, n8);
+      var n10 = cplxExp(n9);
+      var n11 = cplxFromPair(xs[n]);
+      var n12 = cplxMul(n11, n10);
+      var n13 = cplxAdd(acc, n12);
+      acc = n13;
+    }
+    ys.push(acc);
+  }
+
+  return ys;
+}
+</pre>
+<p>The full FFT divides the input into the even numbered and odd numbered points, and the routine is then called recursively on each of them. The key insight of the algorithm then allows the results of the recursive calls to be reassembled into the transform of the entire input signal.</p>
+<pre class="code inBox">
+function fft_generic(xs, inverse) {
+  var N = xs.length;
+
+  // base case: do a brute force dft on length 2
+  if (xs.length == 2)
+    return dft_generic(xs, inverse);
+
+  var ys_half1 = [];
+  var ys_half2 = [];
+
+  var [evens, odds] = separateOddsEvens(xs);
+
+  var Es = fft_generic(evens, inverse);
+  var Os = fft_generic(odds, inverse);
+
+  // the "key" reassembly step
+  for (var k = 0; k < (N >> 1); k++) {
+    var n0 = inverse ? 2 : (-2);
+    var n1 = cplxFromReal(n0);
+    var n2 = cplxMul(n1, cpi);
+    var n3 = cplxMul(n2, j);
+    var n4 = cplxFromReal(k);
+    var n5 = cplxMul(n3, n4);
+    var n6 = cplxFromReal(N);
+    var n7 = cplxDiv(n5, n6);
+    var R = cplxExp(n7);
+    var Ek = Es[k];
+    var Ok = Os[k];
+    var ROk = cplxMul(R, Ok);
+    var yk1 = cplxAdd(Ek, ROk);
+    var yk2 = cplxSub(Ek, ROk);
+    ys_half1.push(yk1);
+    ys_half2.push(yk2);
+  }
+
+  var ys = ys_half1.concat(ys_half2);
+  return ys;
+}
+</pre>
+
 <h4>Parsing expressions</h4>
-<h4>Implementing FFT</h4>
 <h3>Demo</h3>
 <hr />
 <iframe class="inBox" width="560" height="315" src="https://www.youtube.com/embed/yvKetX5DXBc?si=BhuVZoEzjUXOsFkt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+<div id="tryFtvtDiv">
+ <a class="nft" href="ftvt.html" id="tryFtvt"> » Try ftvt yourself « </a>
+</div>
 `;
 
 let c2_ih = c2.innerHTML;
